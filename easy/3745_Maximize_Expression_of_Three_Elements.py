19eda31ca16b0a382683d056861d278045c2665f
Mari Li <marylight.music@yandex.ru>
2026-04-22 00:01:55 +0300

    perf: optimize to single-pass solution without sorting

--- easy/3745_Maximize_Expression_of_Three_Elements.py
+++ easy/3745_Maximize_Expression_of_Three_Elements.py
@@ -1,8 +1,18 @@
 from typing import List
+from math import inf
 
+def maximize_expression_of_three(nums: List[int]) -> int:
+    max_num1 = -inf
+    max_num2 = -inf
+    min_num = inf
 
-def maximizeExpressionOfThree(nums: List[int]) -> int:
-    nums.sort()
-    return nums[-2] + nums[-1] - nums[0]
+    for num in nums:
+        if num >= max_num1:
+            max_num2 = max_num1
+            max_num1 = num
+        elif num > max_num2:
+            max_num2 = num
 
+        min_num = min(min_num, num)
 
+    return max_num1 + max_num2 - min_num
